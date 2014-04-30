d763eadf6d9955ea4342a5c14b43c8dc47085b03
Daniel Lawrence <dannyla@linux.com>
2014-04-30 21:38:44 +1000

    gold plating

--- main.py
+++ main.py
@@ -8,11 +8,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def get_my_ip():
-    xip = request.headers.get('X-Forwarded-For')
-    if xip:
-        return jsonify({'ip': xip}), 200
-        return jsonify({'ip': request.remote_addr}), 200
+    ip = request.headers.get('X-Forwarded-For')
+    if not ip:
+        ip = request.remote_addr
 
+    if 'json' in request.args:
+        return jsonify({'ip': ip}), 200
+    elif 'yaml' in request.args:
+        return "---\nip: %s\n" % ip
+    elif 'plain' in request.args or 'txt' in request.args \
+         or 'text' in request.args:
+        return ip, 200
+    elif 'bash' in request.args or 'zsh' in request.args \
+         or 'ksh' in request.args:
+        return "IP=%s" % ip, 200
+
+    return jsonify({'ip': ip}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
+#
